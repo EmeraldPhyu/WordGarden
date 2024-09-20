cf3059e53d0be490414be0376c7bba584c96311a
Emerald phyu hnin htway <phyuhnin.htway@dzhintl.com>
2024-09-20 15:04:17 +0800

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Emerald on 20/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
